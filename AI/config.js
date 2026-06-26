cd164939e2c0bda8c6917348d94e0ff92f0a65a7
REN11111-a <2307158152@qq.com>
2026-06-27 00:37:38 +0800

    Create config.js

---
+++ AI/config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    API_KEY: 'sk-ibqcxirnvnnxouhffbddfebxsurwvasktkyaoejbaevywnwb',  
+    API_URL: 'https://api.siliconflow.cn/v1/chat/completions',
+    MODEL_NAME: 'deepseek-ai/DeepSeek-V3.2'
+};
