da22a366a1bc965ac79151dca41ff4d0a85bd110
REN11111-a <2307158152@qq.com>
2026-06-27 14:52:31 +0800

    Delete AI/config.js

--- AI/config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const CONFIG = {
-    API_KEY: 'sk-muckmrqiqzwjeksgwbjdhdzfnkypbnfxintrrrbdjcczrioi',  
-    API_URL: 'https://api.siliconflow.cn/v1/chat/completions',
-    MODEL_NAME: 'deepseek-ai/DeepSeek-V3.2'
-};
