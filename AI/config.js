4afce535ae351c0775765fde72770248cba6a655
REN11111-a <2307158152@qq.com>
2026-06-27 13:02:46 +0800

    Update config.js

--- AI/config.js
+++ AI/config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-    API_KEY: 'sk-ibqcxirnvnnxouhffbddfebxsurwvasktkyaoejbaevywnwb',  
+    API_KEY: 'sk-muckmrqiqzwjeksgwbjdhdzfnkypbnfxintrrrbdjcczrioi',  
     API_URL: 'https://api.siliconflow.cn/v1/chat/completions',
     MODEL_NAME: 'deepseek-ai/DeepSeek-V3.2'
 };
